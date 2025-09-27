b0ee385eb5aaa7bae4ad042c17992688d6836f79
devtescode <teslimagboola09@gmail.com>
2025-09-27 12:50:21 +0100

    create user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Route
           path="/userdashboard"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute> 
               <Userdashboard />
             </ProtectedRoute>
           }
