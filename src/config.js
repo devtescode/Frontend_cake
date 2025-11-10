0393341fb70e87954fa20900e6a8be7f830b6072
devtescode <teslimagboola09@gmail.com>
2025-11-10 15:20:19 +0100

    update routes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const isProduction = window.location.hostname !== 'localhost';
 
 export const baseURL = isProduction 
-  ? ''   
+  ? 'https://backend-cake-e3j6.onrender.com'   
   : 'http://localhost:4500';  
 
 
